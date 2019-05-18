005a4273db8bc5b2d6789f8decfad5900f3a33c0
kyoichiro <kyoichiro.kobayashi@ams.eng.osaka-u.ac.jp>
2019-05-19 00:09:12 +0900

    Demo: Reading Sound from mp3 if label == A

--- muscle_news_iOS_front/ReadingSound.swift
+++ muscle_news_iOS_front/ReadingSound.swift
@@ -12,31 +12,31 @@ import AVFoundation
 class ReadingSound: UIViewController {
 
     var audioPlayer: AVAudioPlayer!
+    @IBOutlet weak var testLabel: UILabel!
+    var text1: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // mp3音声(SOUND.mp3)の再生
-        playSound(name: "")
+        let TrainingJudgement =  TrainingJudgement_demo()
+        
+        if(TrainingJudgement.A(name: text1 as! String)) { playSound(name: "test") }
     }
 
 }
 
 extension ReadingSound: AVAudioPlayerDelegate {
     func playSound(name: String) {
+        print(name)
         guard let path = Bundle.main.path(forResource: name, ofType: "mp3") else {
             print("音源ファイルが見つかりません")
             return
         }
         
         do {
-            // AVAudioPlayerのインスタンス化
+            
             audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-            
-            // AVAudioPlayerのデリゲートをセット
             audioPlayer.delegate = self
-            
-            // 音声の再生
             audioPlayer.play()
         } catch {
         }
