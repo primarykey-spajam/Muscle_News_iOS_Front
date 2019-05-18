262870ab53d7f5b25decb22e71747c9aae7c1b54
kyoichiro <kyoichiro.kobayashi@ams.eng.osaka-u.ac.jp>
2019-05-18 18:59:42 +0900

    create menu storyboard

---
+++ muscle_news_iOS_front/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  muscle_news_iOS_front
+//
+//  Created by admin on 2019/05/18.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
