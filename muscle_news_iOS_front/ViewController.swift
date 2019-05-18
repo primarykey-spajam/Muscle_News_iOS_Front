290f5fce4a0f16812ace4971f1583cea1657fca5
kyoichiro <kyoichiro.kobayashi@ams.eng.osaka-u.ac.jp>
2019-05-18 18:35:47 +0900

    feat: create project

---
+++ muscle_news_iOS_front/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  muscle_news_iOS_front
+//
+//  Created by admin on 2019/05/18.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ muscle_news_iOS_frontTests/muscle_news_iOS_frontTests.swift
@@ -0,0 +1,34 @@
+//
+//  muscle_news_iOS_frontTests.swift
+//  muscle_news_iOS_frontTests
+//
+//  Created by admin on 2019/05/18.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import XCTest
+@testable import muscle_news_iOS_front
+
+class muscle_news_iOS_frontTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
